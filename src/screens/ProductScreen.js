701963d028cf83961cd1ec19faeded8a990f2928
Shahar Zidon <shaharsmacbookpro@Shahars-MacBook-Pro.local>
2022-11-16 10:38:38 +0200

    Fixed issued with screens

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -41,7 +41,7 @@ const ProductScreen = ({ history, match }) => {
             dispatch(listProductDetails(match.params.id))
             dispatch({ type: PRODUCT_CREATE_REVIEW_RESET })
         }
-    }, [dispatch, match, successProductReview])
+    }, [dispatch, product._id, match, successProductReview])
 
     const addToCartHandler = () => {
         history.push(`/cart/${match.params.id}?qty=${qty}`)
